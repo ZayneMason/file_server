2481d2c0ddab42cddb9e540714d60c1a0638d059
Mason <masonderolo@gmail.com>
2022-04-18 15:25:30 -0600

    Updated erb files

--- server.rb
+++ server.rb
@@ -3,14 +3,27 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 get '/' do
-  @files = Dir.glob("public/*").map {|file| File.basename(file)}.sort do |a, b|
-    if ('0'..'9').cover?(a.split) && ('0'..'9').cover?(b.split)
-      a.scan("/\^[0-9]/").to_i <=> b.scan("/\^[0-9]/").to_i
-    else
-      a <=> b
-    end
-  end
+  @files = Dir.glob("public/*").map {|file| File.basename(file)}.sort_by{ |name| [name[/\d+/].to_i ? name[/\d+/].to_i : name, name] }
 
   @files.reverse! if params[:sort] == "desc"
   erb :files
+end
+
+get '/upload' do
+  erb :upload
+end
+
+post '/upload' do
+  # Check if user uploaded a file
+  if params[:image] && params[:image][:filename]
+    filename = params[:image][:filename]
+    file = params[:image][:tempfile]
+    path = "./public/#{filename}"
+
+    # Write file to disk
+    File.open(path, 'wb') do |f|
+      f.write(file.read)
+    end
+  end
+  redirect '/'
 end
