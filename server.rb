b6a1cb5eaab3775907e810321763e52c04fa216b
ZayneMason <zayne.c.mason@gmail.com>
2022-04-18 01:38:37 -0500

    updated sort

--- server.rb
+++ server.rb
@@ -3,7 +3,14 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 get '/' do
-  @files = Dir.glob("public/*").map {|file| File.basename(file)}.sort
+  @files = Dir.glob("public/*").map {|file| File.basename(file)}.sort do |a, b|
+    if ('0'..'9').cover?(a.split) && ('0'..'9').cover?(b.split)
+      a.scan("/\^[0-9]/").to_i <=> b.scan("/\^[0-9]/").to_i
+    else
+      a <=> b
+    end
+  end
+
   @files.reverse! if params[:sort] == "desc"
   erb :files
 end
